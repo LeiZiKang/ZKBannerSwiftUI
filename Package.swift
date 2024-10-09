d28bfcadf09aa9bb8b9dd7073f6da75e21b3c6be
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-10-09 20:26:46 +0800

    实现图片轮播

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "ZKBannerSwiftUI",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZKBannerSwiftUI",
             targets: ["ZKBannerSwiftUI"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", .upToNextMajor(from: "3.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZKBannerSwiftUI"),
+            name: "ZKBannerSwiftUI",
+            dependencies: [
+                .product(name: "SDWebImageSwiftUI", package: "SDWebImageSwiftUI")
+            ]
+        ),
 
     ]
 )
--- Sources/ZKBannerSwiftUI/ZKBannerSwiftUI.swift
+++ Sources/ZKBannerSwiftUI/ZKBannerSwiftUI.swift
@@ -1,2 +1,79 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+import SDWebImageSwiftUI
+
+public struct ZKBannerView: View {
+    
+    let imageArr: Array<String>
+    let width: CGFloat = UIScreen.main.bounds.width - 40
+    let height: CGFloat = 200
+    @State var offset: CGFloat
+    @State var currentIndex: Int
+    
+    public init(imageArr: Array<String>, offset: CGFloat = .zero, currentIndex: Int = 0) {
+        self.imageArr = imageArr
+        self.offset = offset
+        self.currentIndex = currentIndex
+    }
+    
+    public var body: some View {
+        HStack{
+            ForEach(getImges(), id: \.self) { img in
+                WebImage(url: URL(string: img))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: width, height: img == imageArr[currentIndex] ? height : height * 0.8)
+                    .clipped()
+            }
+        }
+        .offset(x: offset)
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+                    offset = value.translation.width
+                })
+                .onEnded({ value in
+                    var newIndex = 0
+                    // 向右拖动，显示上一张
+                    if value.translation.width > 50 {
+                        if currentIndex == 0 {
+                            newIndex = imageArr.count - 1
+                        } else {
+                            newIndex = currentIndex - 1
+                        }
+                    }
+                    // 向左拖动，显示下一张
+                    if value.translation.width < 50 {
+                        if currentIndex == imageArr.count - 1 {
+                            newIndex = 0
+                        } else {
+                            newIndex = currentIndex + 1
+                        }
+                    }
+                    
+                    withAnimation(.linear(duration: 0.5)) {
+                        currentIndex = newIndex
+                        offset = .zero
+                    }
+                })
+        )
+    }
+    
+    func getImges() -> [String] {
+        let currentImage = imageArr[currentIndex]
+        let preImage = currentIndex == 0 ? imageArr.last! : imageArr[currentIndex - 1]
+        let nextImage = currentIndex == imageArr.count - 1 ? imageArr.first! : imageArr[currentIndex + 1]
+        return [preImage, currentImage, nextImage]
+    }
+}
+
+#Preview {
+    let images = [
+         "https://images.unsplash.com/photo-1720048171596-6a7c81662434?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+         "https://img0.baidu.com/it/u=597753977,1250737874&fm=253&fmt=auto&app=120&f=JPEG?w=801&h=500",
+         "https://img1.baidu.com/it/u=1795072984,4227544674&fm=253&fmt=auto&app=120&f=JPEG?w=654&h=363",
+         "https://i2.hdslb.com/bfs/archive/b4c0c3907e1f64c2de50edb35a7524d3af48e0f8.jpg"
+     ]
+    ZKBannerView(imageArr: images)
+}
